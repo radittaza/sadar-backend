d2f886712140a8ac521b02b9cb55013a22c300c5
radittaza <radityaazanata028@gmail.com>
2025-12-13 20:20:44 +0700

    fix sqlite path for render

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./sadar.db"
+DATABASE_URL = "sqlite:////tmp/sadar.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
